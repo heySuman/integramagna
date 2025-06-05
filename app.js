d93725d642720e81d16918b847d6f1703f997b8a
Shrestha Suman <iamsuman066@gmail.com>
2025-06-05 10:58:15 +0545

    add the initial files: landing page, marquee, about

---
+++ app.js
@@ -0,0 +1,9 @@
+// const svgPathText = document.getElementById("marqueeText");
+
+// function marquee() {
+//   for (let i = 0; i < 100; i++) {
+//     svgPathText.startOffset = i;
+//   }
+// }
+
+// marquee();
